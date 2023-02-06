cfbd530f46c7270e31bf1d1aa0667141bcc2cbdb
Akrosh Tiwari <metiwariakrosh02@gmail.com>
2023-02-06 14:15:13 +0545

    mobile responsiveness adding breakpoint

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
 const toggleBtn = document.querySelector(".toggle-btn");
-const navbar = document.querySelector("#navbar");
+const navbar1 = document.querySelector("#navbar");
 
 toggleBtn.addEventListener("click", function () {
-    navbar.classList.toggle("active");
+    navbar1.classList.toggle("active");
 });
